58f1a5c6807f2b24dd67b17a84988f95a9b6661c
WladimirWagner <wladimir.wagner@notium.de>
2024-11-27 11:46:43 +0100

    added authenticator

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Amplify } from "aws-amplify";
 import "./app.css";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import outputs from "@/amplify/src/amplify_outputs.json";
+import outputs from "@/amplify_outputs.json";
 
 Amplify.configure(outputs);
 
