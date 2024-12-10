823a904a1b96043390993ced814a22508f912cdb
WladimirWagner <wladimir.wagner@notium.de>
2024-12-10 09:20:04 +0100

    added organizations and members

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,11 +6,21 @@ import { defineAuth } from "@aws-amplify/backend";
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to my app!",
+      verificationEmailBody: (createCode) => `Use this code to confirm your account: ${createCode()}`,
+      userInvitation: {
+        emailSubject: "Welcome to my app!",
+        emailBody: (user, code) =>
+          `We're happy to have you! You can now login with username ${user()} and temporary password ${code()}`, 
+      },
+    },
   },
   userAttributes: {
-    // Optional: Definiere zusätzliche Benutzerattribute
+    "custom:roles": {
+      dataType: "String",
+      mutable: true,
+    }
   },
-  // Definiere die Cognito-Gruppen
-  groups: ['Admin', 'Member'],
 });
